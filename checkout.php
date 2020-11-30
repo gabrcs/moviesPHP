67b044e371c3f1c9faec2134975f726723ec0063
gabrcs <68124379+gabrcs@users.noreply.github.com>
2020-11-30 15:53:30 -0300

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,28 @@
+<?php 
+
+?>
+<DOCTYPE! html>
+    <head>
+
+        <title> Movies PHP</title>
+        <link rel="stylesheet" type="text/css" href="Style/style.css">
+        <meta charset="UTF-8">
+
+    </head>
+
+    <body> 
+
+        <header>  
+           <a href="index.php"> <img src="Image/moviesPHP.png" id="headerImg"> </a>
+        </header>
+
+        <div class="container"> 
+            <div class="centerDiv">
+
+
+
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+
+?>
+
+<DOCTYPE! html>
+    <head>
+
+        <title> Movies PHP</title>
+        <link rel="stylesheet" type="text/css" href="Style/style.css">
+        <meta charset="UTF-8">
+
+    </head>
+
+    <body> 
+
+        <header>  
+           <a href="index.php"> <img src="Image/moviesPHP.png" id="headerImg"> </a>
+        </header>
+
+        <div class="container"> 
+            <div class="centerDiv">
+                <div class="movieDiv"> 
+                    <p id="releaseText">Lançamento </p>
+                    <p id="movieNameText">PHP Developers </p>
+                    <img src="Image/PHPvideo.png" id="content"> <br>
+                    <a href="checkout.php"> <img src="Image/buyNow.png" id="buyNow"> </a>
+                </div>
+                <div class="movieDiv2"> 
+                    <p id="releaseText2">Lançamento </p>
+                    <p id="movieNameText2">A caminhada </p>
+                    <img src="Image/RUNvideo.png" id="content"> <br>
+                    <a href="checkout.php"><img src="Image/buyNow.png" id="buyNow2"> </a>
+                </div>
+            </div>
+        </div>
+
+        <footer> 
+
+        </footer>
+
+    </body>
+</html>
